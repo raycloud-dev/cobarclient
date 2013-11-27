fcdd22f4434e3d0b76411b0e7bf469e406692241
kevin <guyan.data@gmail.com>
2013-11-27 12:53:01 +0800

    1 修复了当 functions 为空的时候的 null 指针错误 2 增加了 dao support 的 template 自动注入，这样就不用每个 dao 单独配置 template 了

--- src/main/java/com/alibaba/cobarclient/MysdalCobarSqlMapClientDaoSupport.java
+++ src/main/java/com/alibaba/cobarclient/MysdalCobarSqlMapClientDaoSupport.java
@@ -16,10 +16,13 @@
 package com.alibaba.cobarclient;
 
 import com.ibatis.sqlmap.client.SqlMapExecutor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.orm.ibatis.SqlMapClientCallback;
+import org.springframework.orm.ibatis.SqlMapClientTemplate;
 import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
 
+import javax.annotation.PostConstruct;
 import java.sql.SQLException;
 import java.util.Collection;
 
@@ -32,6 +35,13 @@ import java.util.Collection;
  * @since 1.0
  */
 public class MysdalCobarSqlMapClientDaoSupport extends SqlMapClientDaoSupport {
+    @Autowired
+    private SqlMapClientTemplate sqlMapClientTemplate = new SqlMapClientTemplate();
+
+    @PostConstruct
+    public void init() {
+        setSqlMapClientTemplate(sqlMapClientTemplate);
+    }
 
     public int batchInsert(final String statementName, final Collection<?> entities)
             throws DataAccessException {
